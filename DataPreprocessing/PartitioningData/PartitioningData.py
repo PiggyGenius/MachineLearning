0979ac182cf7da340f4ba19d98af1966299e9aca
piggygenius <ludo.itrts@gmail.com>
2016-11-05 20:09:43 +0100

    Data splitting into test and train sets

---
+++ DataPreprocessing/PartitioningData/PartitioningData.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3.5
+# -*-coding:Utf-8 -*
+import numpy as np
+import pandas as pd
+from sklearn.cross_validation import train_test_split
+
+wine_url = "https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data"
+df_wine = pd.read_csv(wine_url,header=None)
+columns = ["Class label","Alcohol","Malic acid","Ash","Alcalinity of ash","Magnesium","Total phenols","Flavanoids","Nonflavanoid phenols","Proanthocyanins","Color intensity","Hue","OD280/OD315 of diluted wines","Proline"]
+df_wine.columns = columns 
+
+print("Class labels: ", np.unique(df_wine["Class label"]),'\n')
+print(df_wine.head())
+
+# First column is class label
+classes =  df_wine.iloc[:, 0].values
+# the rest is the data
+values =  df_wine.iloc[:, 1:].values
+
+# 30 percent of the dataset will the test set
+train_values, test_values, train_classes, test_classes = train_test_split(values,classes,test_size=0.3,random_state=0)
+
