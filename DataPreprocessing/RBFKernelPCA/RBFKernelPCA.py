2172129c96c70b7952f161d8277d53fdaf328530
PiggyGenius <ludo.itrts@gmail.com>
2016-12-28 13:00:25 +0100

    Adding RBF kernel PCA and an example seperating half moon shapes, non-linearly seperable dataset

---
+++ DataPreprocessing/RBFKernelPCA/RBFKernelPCA.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3.5
+# -*-coding:Utf-8 -*
+import numpy as np
+from scipy.spatial.distance import pdist, squareform
+from scipy.linalg import eigh
+from scipy import exp
+
+def RBF_Kernel_PCA(values, gamma, n_components):
+    """
+    RBF Kernel PCA implementation.
+
+    Parameters
+    ----------------------------------------
+    values: {NumPy ndarray}, shape = [n_samples, n_features]
+    gamma: float
+        Tuning paramater of the RBF kernel
+    n_components: int
+        Number of principal comonents to return
+
+    Returns
+    ----------------------------------------
+    pca_values: {NumPy ndarray}, shape = [n_samples, k_features]
+        Projected dataset
+    """
+
+    # Calculate pairwise squared Euclidian distances in the MxN dimensional dataset
+    square_distance = pdist(values, 'sqeuclidean')
+
+    # Convert pairwise distances into a square matrix
+    square_distance_matrix = squareform(square_distance)
+
+    # Compute the symmetric kernel matrix
+    kernel = exp(-gamma * square_distance_matrix)
+
+    # Center the kernel matrix
+    N = kernel.shape[0]
+    one_n = np.ones((N,N)) / N
+    kernel = kernel - one_n.dot(kernel) - kernel.dot(one_n) + one_n.dot(kernel).dot(one_n)
+
+    # Obtaining eigenpairs from the centered kernel matrix, numpy.eigh returns them sorted
+    eigenvalues, eigenvectors = eigh(kernel)
+
+    # Collect the top K eigenvectors, projected samples
+    pca_values = np.column_stack((eigenvectors[:,-i] for i in range(1,n_components + 1)))
+
+    return pca_values
---
+++ DataPreprocessing/RBFKernelPCA/SeperatingHalfMoonShapes.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3.5
+# -*-coding:Utf-8 -*
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_moons
+from sklearn.decomposition import PCA
+from matplotlib.ticker import FormatStrFormatter
+from RBFKernelPCA import RBF_Kernel_PCA
+
+# We create dataset of two half shapes moons
+values, classes = make_moons(n_samples = 100, random_state = 123)
+plt.scatter(values[classes == 0, 0], values[classes == 0, 1], color = 'red', marker = '^', alpha = 0.5)
+plt.scatter(values[classes == 1, 0], values[classes == 1, 1], color = 'blue', marker = 'o', alpha = 0.5)
+plt.show()
+
+# These two half-moon shapes are not linearly seperable and our goal is to unfold the half-moons via kernel PCA so that the datasets can be used with linear classifiers.
+# First, we see what the datasets looks like if we project it onto the principal components via standard PCA.
+
+# We use PCA on the dataset
+pca = PCA(n_components = 2)
+pca_values = pca.fit_transform(values)
+fig, ax = plt.subplots(nrows = 1, ncols = 2, figsize = (7,3))
+
+# We display the data with a shift, to better visualize the class overlap
+ax[0].scatter(pca_values[classes == 0, 0], pca_values[classes == 0, 1], color = 'red', marker = '^', alpha = 0.5)
+ax[0].scatter(pca_values[classes == 1, 0], pca_values[classes == 1, 1], color = 'blue', marker = 'o', alpha = 0.5)
+ax[1].scatter(pca_values[classes == 0, 0], np.zeros((50,1)) + 0.02, color = 'red', marker = '^', alpha = 0.5)
+ax[1].scatter(pca_values[classes == 1, 0], np.zeros((50,1)) - 0.02, color = 'blue', marker = 'o', alpha = 0.5)
+ax[0].set_xlabel('PC1')
+ax[0].set_ylabel('PC2')
+ax[1].set_ylim([-1, 1])
+ax[1].set_yticks([])
+ax[1].set_xlabel('PC1')
+plt.show()
+
+# Now we use kernel PCA
+kernel_pca_values = RBF_Kernel_PCA(values, gamma = 15,n_components = 2)
+fig, ax = plt.subplots(nrows = 1, ncols = 2, figsize = (7,3))
+ax[0].scatter(kernel_pca_values[classes == 0, 0], kernel_pca_values[classes == 0, 1], color = 'red', marker = '^', alpha = 0.5)
+ax[0].scatter(kernel_pca_values[classes == 1, 0], kernel_pca_values[classes == 1, 1], color = 'blue', marker = 'o', alpha = 0.5)
+ax[1].scatter(kernel_pca_values[classes == 0, 0], np.zeros((50,1)) + 0.02, color = 'red', marker = '^', alpha = 0.5)
+ax[1].scatter(kernel_pca_values[classes == 1, 0], np.zeros((50,1)) - 0.02, color = 'blue', marker = 'o', alpha = 0.5)
+ax[0].set_xlabel('PC1')
+ax[0].set_ylabel('PC2')
+ax[1].set_ylim([-1, 1])
+ax[1].set_yticks([])
+ax[1].set_xlabel('PC1')
+ax[0].xaxis.set_major_formatter(FormatStrFormatter('%0.1f'))
+ax[1].xaxis.set_major_formatter(FormatStrFormatter('%0.1f'))
+plt.show()
